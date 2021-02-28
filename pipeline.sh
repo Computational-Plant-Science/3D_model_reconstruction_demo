1e8f4852709f432e274ef3922d5212c267827b88
lsx1980 <suxingliu@gmail.com>
2021-02-28 13:55:15 -0500

    update shell bash script

--- pipeline.sh
+++ pipeline.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-'
-Reconstruct 3D root model from image dataset using colmap and SFM.
-'
+#
+# Reconstruct 3D root model from image dataset using colmap and SFM.
+#
 
 
 colmap feature_extractor --image_path /images/ --database_path /images/database.db --SiftExtraction.use_gpu=false
@@ -18,19 +18,4 @@ colmap model_converter --input_path /images/sparse/0 --output_path /images/model
 
 chmod 777 -R /images/
 
-'
-#!/bin/bash
-# progress bar function
-prog() {
-    local w=80 p=$1;  shift
-    # create a string of spaces, then change them to dots
-    printf -v dots "%*s" "$(( $p*$w/100 ))" ""; dots=${dots// /.};
-    # print those dots on a fixed-width space plus the percentage etc. 
-    printf "\r\e[K|%-*s| %3d %% %s" "$w" "$dots" "$p" "$*"; 
-}
-# test loop
-for x in {1..100} ; do
-    prog "$x" still working...
-    sleep .1   # do some work here
-done ; echo
-'
+
