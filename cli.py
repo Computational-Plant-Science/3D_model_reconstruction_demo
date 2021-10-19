e962b82d587199cca1e9dccac6d286b965668b88
Wes Bonelli <wpb3hw@virginia.edu>
2021-10-19 11:52:17 -0400

    update dockerfiles

--- cli.py
+++ cli.py
@@ -44,7 +44,7 @@ def reconstruct(source, output_directory, preprocess, gpu):
 
     # feature extraction
     # last two options prevent memory overconsumption in CPU mode https://colmap.github.io/faq.html#available-functionality-without-gpu-cuda
-    subprocess.run(f"colmap feature_extractor --image_path {source} --database_path {database}" + (" --SiftExtraction.use_gpu=0 --SiftExtraction.num_threads=2 --SiftExtraction.first_octave 0" if not gpu else ''), shell=True)
+    subprocess.run(f"colmap feature_extractor --image_path {source} --database_path {database}" + ('' if gpu else ' --SiftExtraction.use_gpu=0 --SiftExtraction.num_threads=2 --SiftExtraction.first_octave 0'), shell=True)
 
     # feature matching
     # might need to use --SiftMatching.max_num_matches as per https://colmap.github.io/faq.html#feature-matching-fails-due-to-illegal-memory-access
@@ -59,17 +59,16 @@ def reconstruct(source, output_directory, preprocess, gpu):
     subprocess.run(f"colmap model_converter --input_path {join(sparse, '0')} --output_path {join(output_directory, 'sparse.nvm')} --output_type NVM", shell=True)
     subprocess.run(f"colmap model_converter --input_path {join(sparse, '0')} --output_path {join(output_directory, 'sparse.ply')} --output_type PLY", shell=True)
 
-    # (not using VSFM for dense model anymore)
-    # subprocess.run("/opt/code/vsfm/bin/VisualSFM sfm+loadnvm+pmvs " + join(output_directory, 'model.nvm') + " " + join(output_directory, 'dense.nvm'), shell=True)
-
-    # dense model reconstruction
-    subprocess.run(f"mkdir {join(output_directory, 'dense')}", shell=True)
-    subprocess.run(f"colmap image_undistorter --image_path {source} --input_path {join(output_directory, 'sparse', '0')} --output_path {join(output_directory, 'dense')} --output_type COLMAP --max_image_size 2000", shell=True)
-    subprocess.run(f"colmap patch_match_stereo --workspace_path {join(output_directory, 'dense')}" + " --workspace_format COLMAP --PatchMatchStereo.geom_consistency true", shell=True)
-    subprocess.run(f"colmap stereo_fusion --workspace_path {join(output_directory, 'dense')} --workspace_format COLMAP --input_type geometric --output_path {join(output_directory, 'dense.ply')}", shell=True)
-
-    # only required for mesh, not point cloud
-    subprocess.run(f"colmap poisson_mesher --input_path {join(output_directory, 'dense.ply')} --output_path {join(output_directory, 'mesh.ply')}", shell=True)
+    # dense model reconstruction (colmap on GPU, VSFM on CPU)
+    if gpu:
+        subprocess.run(f"mkdir {join(output_directory, 'dense')}", shell=True)
+        subprocess.run(f"colmap image_undistorter --image_path {source} --input_path {join(output_directory, 'sparse', '0')} --output_path {join(output_directory, 'dense')} --output_type COLMAP --max_image_size 2000", shell=True)
+        subprocess.run(f"colmap patch_match_stereo --workspace_path {join(output_directory, 'dense')}" + " --workspace_format COLMAP --PatchMatchStereo.geom_consistency true", shell=True)
+        subprocess.run(f"colmap stereo_fusion --workspace_path {join(output_directory, 'dense')} --workspace_format COLMAP --input_type geometric --output_path {join(output_directory, 'dense.ply')}", shell=True)
+        subprocess.run(f"colmap poisson_mesher --input_path {join(output_directory, 'dense.ply')} --output_path {join(output_directory, 'mesh.ply')}", shell=True)
+    else:
+        Path()
+        subprocess.run("/opt/code/vsfm/bin/VisualSFM sfm+loadnvm+pmvs " + join(output_directory, 'model.nvm') + " " + join(output_directory, 'dense.nvm'), shell=True)
 
     end = time.time()
     print("Finished in " + str(timedelta(seconds=(end - start))))
