b6e8764f969d1ff4cd4367907012d72c74ca5748
Wes Bonelli <wpb3hw@virginia.edu>
2021-10-19 11:55:12 -0400

    update plantit config

--- plantit_pipeline.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-colmap feature_extractor --image_path $INPUT --database_path $OUTPUT/database.db
-colmap exhaustive_matcher --database_path $OUTPUT/database.db
-colmap mapper --image_path $INPUT --database_path $OUTPUT/database.db --output_path $OUTPUT
-colmap model_converter --input_path $OUTPUT/0 --output_path $OUTPUT/model.ply --output_type PLY
