c9bd180ae7f5b4e41d306fcffa2fd273774bb591
Wes Bonelli <wpb3hw@virginia.edu>
2021-10-20 20:28:57 -0400

    fix bool arg handling

--- pipeline.py
+++ pipeline.py
@@ -10,11 +10,14 @@ Author-email: suxingliu@gmail.com
 Usage: python3 pipeline.py <input directory> -o <output directory> -p <True or False> -g <True or False>
 
 Arguments:
-("-i", "--input_diirectory", required = True, help = "where to find input image files")
-("-o", "--output_directory", required = True, help = "where to write output files")
-("-p", "--preprocess", required = False, help = "whether to preprocess images, defaults to False")
-("-g", "--gpu", required = False, default = False, help = "whether to use GPUs")
+("-i", "--input_directory", required=True, help="where to find input image files")
+("-o", "--output_directory", required=True, help="where to write output files")
+("-s", "--segmentation", required=False, default=False, help="whether to apply root segmentation to images")
+("-bd", "--blur_detection", required=False, default=False, help="whether to apply blur detection and omit blurry images")
+("-gc", "--gamma_correction", required=False, default=False, help="whether to apply gamma correction to images"
+("-g", "--gpu", required=False, default=False, help="whether to use GPUs")
 """
+import distutils
 import multiprocessing
 import subprocess
 
@@ -32,12 +35,18 @@ from pathlib import Path
 from model_preprocess.bbox_seg import foreground_substractor
 
 
-def reconstruct(source: str, output_directory: str, preprocess: bool, gpu: bool):
-    if not os.path.exists(source):
-        raise ValueError("Path does not exist!")
+def reconstruct(
+        input_directory,
+        output_directory,
+        segmentation,
+        blur_detection,
+        gamma_correction,
+        gpu):
+    if not os.path.exists(input_directory):
+        raise ValueError("Input directory does not exist!")
 
-    if preprocess:
-        paths = [join(source, file) for file in listdir(source) if isfile(join(source, file))]
+    if segmentation:
+        paths = [join(input_directory, file) for file in listdir(input_directory) if isfile(join(input_directory, file))]
         print("Preprocessing " + str(len(paths)) + " files:")
         pprint.pprint(paths)
         preprocessed_directory = Path('preprocessed')
@@ -46,12 +55,14 @@ def reconstruct(source: str, output_directory: str, preprocess: bool, gpu: bool)
         with multiprocessing.Pool(processes=multiprocessing.cpu_count()) as pool:
             pool.starmap(foreground_substractor, path_args)
 
+    # TODO blur detection and gamma correction
+
     start = time.time()
     database = join(output_directory, 'database.db')
 
     # feature extraction
     # last two options prevent memory overconsumption in CPU mode https://colmap.github.io/faq.html#available-functionality-without-gpu-cuda
-    subprocess.run("colmap feature_extractor --image_path " + source + " --database_path " + database + ('' if gpu else ' --SiftExtraction.use_gpu=0 --SiftExtraction.num_threads=2 --SiftExtraction.first_octave 0'), shell=True)
+    subprocess.run("colmap feature_extractor --image_path " + input_directory + " --database_path " + database + ('' if gpu else ' --SiftExtraction.use_gpu=0 --SiftExtraction.num_threads=2 --SiftExtraction.first_octave 0'), shell=True)
 
     # feature matching
     # might need to use --SiftMatching.max_num_matches as per https://colmap.github.io/faq.html#feature-matching-fails-due-to-illegal-memory-access
@@ -60,16 +71,18 @@ def reconstruct(source: str, output_directory: str, preprocess: bool, gpu: bool)
     # build sparse model
     sparse = join(output_directory, 'sparse')
     subprocess.run("mkdir " + sparse, shell=True)
-    subprocess.run("colmap mapper --database_path " + database + " --image_path " + source + " --output_path " + sparse, shell=True)
+    subprocess.run("colmap mapper --database_path " + database + " --image_path " + input_directory + " --output_path " + sparse, shell=True)
 
     # convert models
     subprocess.run("colmap model_converter --input_path " + join(sparse, '0') + " --output_path " + join(output_directory, 'sparse.nvm') + " --output_type NVM", shell=True)
     subprocess.run("colmap model_converter --input_path " + join(sparse, '0') + " --output_path " + join(output_directory, 'sparse.ply') + " --output_type PLY", shell=True)
 
+    # TODO might need to use 'images' folder as described by Suxing?
+
     # dense model reconstruction (colmap on GPU, VSFM on CPU)
     if gpu:
         subprocess.run("mkdir " + join(output_directory, 'dense'), shell=True)
-        subprocess.run("colmap image_undistorter --image_path " + source + " --input_path " + join(output_directory, 'sparse', '0') + " --output_path " + join(output_directory, 'dense') + " --output_type COLMAP --max_image_size 2000", shell=True)
+        subprocess.run("colmap image_undistorter --image_path " + input_directory + " --input_path " + join(output_directory, 'sparse', '0') + " --output_path " + join(output_directory, 'dense') + " --output_type COLMAP --max_image_size 2000", shell=True)
         subprocess.run("colmap patch_match_stereo --workspace_path " + join(output_directory,'dense') + " --workspace_format COLMAP --PatchMatchStereo.geom_consistency true", shell=True)
         subprocess.run("colmap stereo_fusion --workspace_path " + join(output_directory, 'dense') + " --workspace_format COLMAP --input_type geometric --output_path " + join(output_directory, 'dense.ply'), shell=True)
         subprocess.run("colmap poisson_mesher --input_path " + join(output_directory, 'dense.ply') + " --output_path " + join(output_directory, 'mesh.ply'), shell=True)
@@ -80,17 +93,25 @@ def reconstruct(source: str, output_directory: str, preprocess: bool, gpu: bool)
     print("Finished in " + str(timedelta(seconds=(end - start))))
 
 
+# adapted from https://stackoverflow.com/a/43357954/6514033
+def str2bool(v):
+    return bool(distutils.util.strtobool(v))
+
+
 if __name__ == '__main__':
     ap = argparse.ArgumentParser()
     ap.add_argument("-i", "--input_directory", required=True, help="where to find input images")
     ap.add_argument("-o", "--output_directory", required=False, default=".", help="where to write results")
-    ap.add_argument("-p", "--preprocess", required=False, default=False, help="whether to preprocess images")
-    ap.add_argument("-g", "--gpu", required=False, default=False, help="whether to use GPUs")
+    ap.add_argument("-s", "--segmentation", type=str2bool, nargs='?', const=True, default=False, help="whether to apply root segmentation to images")
+    ap.add_argument("-bd", "--blur_detection", type=str2bool, nargs='?', const=True, default=False, help="whether to apply blur detection and omit blurry images")
+    ap.add_argument("-gc", "--gamma_correction", type=str2bool, nargs='?', const=True, default=False, help="whether to apply gamma correction to images")
+    ap.add_argument("-g", "--gpu", type=str2bool, nargs='?', const=True, default=False, help="whether to use GPUs")
     args = vars(ap.parse_args())
 
-    source = args["input_directory"]
-    target = args["output_directory"]
-    preprocess = args["preprocess"]
-    gpu = args["gpu"]
-
-    reconstruct(source, target, preprocess, gpu)
+    reconstruct(
+        args["input_directory"],
+        args["output_directory"],
+        bool(args["segmentation"]),
+        bool(args["blur_detection"]),
+        bool(args["gamma_correction"]),
+        bool(args["gpu"]))
